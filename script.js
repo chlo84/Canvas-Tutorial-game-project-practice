9de130b2073953863c76ca0c0dc32ba9caefb829
Chloe <chloe84@msn.com>
2022-01-24 15:19:22 -0800

    canvas tutorial

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('my canvas');
+const context = canvas.getContext('2d');
+context.beginPath = 
+context.fillStyle = 'red';
+context.fillRect = 50, 50, 100, 100;
+context.closePath = 
+
+context.beginPath = 
+context.fillStyle = 'green';
+context.arc = 240, 160, 20, 0, Math.PI*2, false;
+context.closePath = 
